9b16be83215df4f93d9a710fbf97fbfa77ce8fc5
Antonio Villagomez <avillagomez@csumb.edu>
2017-09-21 19:17:48 +0000

    HW2a

--- HW2/index.php
+++ HW2/index.php
@@ -1,24 +1,30 @@
 <html>
  <head>
-  <title>Random Images</title>
+  <title>Random Images</title
+  <link rel ="stylesheet" type="text/css" href="style.css"/>
  </head>
  <body>
-  <ul>
+  
+<div id = "kicks">  
+   
+   
 	<?php
 		$shoes = array(
-	         '< img src= "images/airmax.jpg"/>',
-	         '< img src= "images/hyperdunk.jpg"/>',
-	         '< img src= "images/jordan1.jpg"/>',
-	         '< img src= "imagesv/vapormax.jpg"/>');
+	         '< img src= "images/airmax.jpg" title="kicks 1"/>',
+	         '< img src= "images/hyperdunk.jpg" title="kicks 2"/>',
+	         '< img src= "images/jordan1.jpg" title="kicks 3"/>',
+	         '< img src= "imagesv/vapormax.jpg" title="kicks 4"/>');
 	 shuffle($shoes);
+	 $rand = array_slice($shoes,0,4);
+	 echo implode ('',$rand);
 	
-	   for( $i = 0; $i < 4; $i++)
-	      echo "<li style=\"display: inline;\">
-                         <img src=\"$shoes[$i]\" width=\"940\" height=\"600\">
-                       </li>";
+	   // for( $i = 0; $i < 4; $i++)
+	   //    echo "<li style=\"display: inline;\">
+    //                      <img src=\"$shoes[$i]\" width=\"940\" height=\"600\">
+    //                    </li>";
  
 	?>	
- </ul>
+ 
  
  </body>
 </html>
