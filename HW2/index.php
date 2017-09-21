40b0a30f68d5fe89406a2d9887f56977889b5dea
Antonio Villagomez <avillagomez@csumb.edu>
2017-09-21 20:25:35 +0000

    minor change

--- HW2/index.php
+++ HW2/index.php
@@ -2,17 +2,16 @@
  <head>
   <title>Random Images</title>
   <style>
-            @import url("css/styles.css");
+        @import url("css/styles.css");
    </style>
  </head>
  <body>
-
+<div class = "ow">
    <h1>OFF-WHITE NIKES</h1>
-   
+</div>   
   
 	<?php
-		$shoes = array("airmax","hyperdunk","vapormax","jordan1"
-	         );
+		$shoes = array("airmax","hyperdunk","vapormax","jordan1");
 	 shuffle($shoes);
 	 foreach ($shoes as $shoes){
 	  echo "<img src ='images/$shoes.jpg'><br>";
