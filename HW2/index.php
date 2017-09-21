8a50dff59869719dce4cf88fe21aa8fba62dd592
Antonio Villagomez <avillagomez@csumb.edu>
2017-09-21 20:29:29 +0000

    HW

--- HW2/index.php
+++ HW2/index.php
@@ -13,6 +13,7 @@
 	<?php
 		$shoes = array("airmax","hyperdunk","vapormax","jordan1");
 	 shuffle($shoes);
+	 // AS LONG AS THE VARIABLES ARE THE SAME DATA TYPE THIS FOREACH HELPS ITTEREATE THRU THE IMAGES LIKE A FOR LOOP DOES
 	 foreach ($shoes as $shoes){
 	  echo "<img src ='images/$shoes.jpg'><br>";
 	 }
